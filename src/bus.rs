4753fec53931a4e663d863ea046aef62a7a35551
Lucas Clemente Vella <lvella@gmail.com>
2024-05-25 11:03:38 +0100

    Fixed mem access function.

--- src/bus.rs
+++ src/bus.rs
@@ -104,7 +104,8 @@ impl Bus {
         }
     }
 
-    pub fn get_dram_slice(&mut self, range: Range<usize>) -> Result<&mut [u8], Exception> {
+    pub fn get_dram_slice(&mut self, range: Range<u64>) -> Result<&mut [u8], Exception> {
+        let range = (range.start - DRAM_BASE) as usize..(range.end as usize - DRAM_BASE as usize);
         self.dram
             .dram
             .get_mut(range)
